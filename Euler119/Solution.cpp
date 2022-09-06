a18d60fe7296123956853dac09078cf1359fe05d
Николай <nikolai.chichulin@gmail.com>
2022-09-06 12:18:15 +0300

    Updated.

--- Euler119/Solution.cpp
+++ Euler119/Solution.cpp
@@ -7,6 +7,43 @@
 using namespace std;
 
 /// <summary>
+/// Returns sum of two integers represented as vectors.
+/// </summary>
+/// <param name="a">The first integer as a vector.</param>
+/// <param name="b">The second integer as a vector.</param>
+/// <returns></returns>
+vector<int> sum(vector<int> a, vector<int> b) {
+
+    vector<int> ret;
+    int la = static_cast<int>(a.size());
+    int lb = static_cast<int>(b.size());
+    int lmin = min(la, lb);
+    int lmax = max(la, lb);
+    int c = 0;
+    for (int i = 0; i < lmin; i++) {
+        int res = a[i] + b[i] + c;
+        int rem = res % 10;
+        c = res / 10;
+        ret.push_back(rem);
+    }
+
+    if (la != lb) {
+        bool aig = la >= lb;
+        for (int i = lmin; i < lmax; i++) {
+            int res = (aig ? a[i] : b[i]) + c;
+            int rem = res % 10;
+            c = res / 10;
+            ret.push_back(rem);
+        }
+    }
+    if (c > 0) {
+        ret.push_back(c);
+    }
+
+    return ret;
+}
+
+/// <summary>
 /// Returns multiplication of two integers represented as vectors of int.
 /// </summary>
 /// <param name="a">The first integer.</param>
@@ -46,11 +83,12 @@ vector<int> multuply(vector<int> a, vector<int> b) {
     return ret;
 }
 
-
-
-vector<int> power(vector<int> v, int n) {
-
-}
+//vector<int> power(vector<int> v, int n) {
+//    vector<int> ret;
+//    for (int i = 0; i < n; i++) {
+//        ret = multuply(v, v);
+//    }
+//}
 
 int sumOfDigits(int n, int base) {
 
@@ -68,6 +106,105 @@ int sumOfDigits(int n, int base) {
     }
 
     return s;
+}
+
+bool multTest(vector<int> v1, vector<int> v2, vector<int> exp) {
+
+    vector<int> act = multuply(v1, v2);
+
+    if (exp.size() != act.size()) {
+        return false;
+    }
+
+    for (int i = 0; i < exp.size(); i++) {
+        if (exp[i] != act[i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+bool multTests() {
+
+    vector<int> v1 = { 0 };
+    vector<int> v2 = { 5, 2, 3, 2, 9, 1 };
+    vector<int> exp = { 0, 0, 0, 0, 0, 0 };
+    if (!multTest(v1, v2, exp)) {
+        return false;
+    }
+
+    v1 = { 1 };
+    v2 = { 5, 7, 8, 3 };
+    exp = { 5, 7, 8, 3 };
+    if (!multTest(v1, v2, exp)) {
+        return false;
+    }
+
+    v1 = { 5, 7, 8, 3 };
+    v2 = { 1 };
+    exp = { 5, 7, 8, 3 };
+    if (!multTest(v1, v2, exp)) {
+        return false;
+    }
+
+    v1 = { 2 };
+    v2 = { 5, 7, 8, 3 };
+    exp = { 0, 5, 7, 7 };
+    if (!multTest(v1, v2, exp)) {
+        return false;
+    }
+
+    v1 = { 5, 7, 8, 3 };
+    v2 = { 2 };
+    exp = { 0, 5, 7, 7 };
+    if (!multTest(v1, v2, exp)) {
+        return false;
+    }
+
+    v1 = { 3 };
+    v2 = { 5, 7, 8, 3 };
+    exp = { 5, 2, 6, 1, 1 };
+    if (!multTest(v1, v2, exp)) {
+        return false;
+    }
+
+    v1 = { 5, 7, 8, 3 };
+    v2 = { 3 };
+    exp = { 5, 2, 6, 1, 1 };
+    if (!multTest(v1, v2, exp)) {
+        return false;
+    }
+
+    v1 = { 5, 2, 3, 2, 9, 1 };
+    v2 = { 4, 5, 7, 8, 3 };
+    exp = { 0, 5, 0, 3, 6, 3, 3, 5, 4, 7 };
+    if (!multTest(v1, v2, exp)) {
+        return false;
+    }
+
+    v1 = { 4, 5, 7, 8, 3 };
+    v2 = { 5, 2, 3, 2, 9, 1 };
+    exp = { 0, 5, 0, 3, 6, 3, 3, 5, 4, 7 };
+    if (!multTest(v1, v2, exp)) {
+        return false;
+    }
+
+    v1 = { 1, 1, 1, 1, 1 };
+    v2 = { 2, 2, 2, 2, 2, 2, 2, 2, 2 };
+    exp = { 2, 4, 6, 8, 0, 1, 1, 1, 1, 9, 6, 4, 2 };
+    if (!multTest(v1, v2, exp)) {
+        return false;
+    }
+
+    v1 = { 2, 2, 2, 2, 2, 2, 2, 2, 2 };
+    v2 = { 1, 1, 1, 1, 1 };
+    exp = { 2, 4, 6, 8, 0, 1, 1, 1, 1, 9, 6, 4, 2 };
+    if (!multTest(v1, v2, exp)) {
+        return false;
+    }
+
+    return true;
 }
 
 /// <summary>
@@ -141,6 +278,6 @@ void solve(int base) {
 
 int main() {
 
-    solveN(10);
+    cout << (multTests() ? "Passed" : "Failed") << endl;
     return 0;
 }
