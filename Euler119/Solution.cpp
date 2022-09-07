f6080ad8b020ffce5764a187aba2b4b54942a138
Николай <nikolai.chichulin@gmail.com>
2022-09-07 15:29:18 +0300

    Updated.

--- Euler119/Solution.cpp
+++ Euler119/Solution.cpp
@@ -4,16 +4,36 @@
 #include <iostream>
 #include <algorithm>
 #include <map>
+#include <chrono>
 
 using namespace std;
+
+vector<int> powers[1300][500];
+
+vector<int> inverse(vector<int> v) {
+
+    vector<int> ret(v.size());
+    for (int i = 0; i < v.size(); i++) {
+        ret[i] = v[v.size() - 1 - i];
+    }
+    return ret;
+}
+
+void out(vector<int> v) {
+    for (int v : v) {
+        cout << v << ":";
+    }
+    cout << " ";
+}
 
 /// <summary>
 /// Returns sum of two integers represented as vectors.
 /// </summary>
 /// <param name="a">The first integer as a vector.</param>
 /// <param name="b">The second integer as a vector.</param>
+/// <param name="base">The base of the numeral system.</param>
 /// <returns></returns>
-vector<int> sum(vector<int> a, vector<int> b) {
+vector<int> sum(vector<int> a, vector<int> b, int base) {
 
     vector<int> ret;
     int la = static_cast<int>(a.size());
@@ -23,8 +43,8 @@ vector<int> sum(vector<int> a, vector<int> b) {
     int c = 0;
     for (int i = 0; i < lmin; i++) {
         int res = a[i] + b[i] + c;
-        int rem = res % 10;
-        c = res / 10;
+        int rem = res % base;
+        c = res / base;
         ret.push_back(rem);
     }
 
@@ -32,8 +52,8 @@ vector<int> sum(vector<int> a, vector<int> b) {
         bool aig = la >= lb;
         for (int i = lmin; i < lmax; i++) {
             int res = (aig ? a[i] : b[i]) + c;
-            int rem = res % 10;
-            c = res / 10;
+            int rem = res % base;
+            c = res / base;
             ret.push_back(rem);
         }
     }
@@ -49,8 +69,9 @@ vector<int> sum(vector<int> a, vector<int> b) {
 /// </summary>
 /// <param name="a">The first integer.</param>
 /// <param name="b">The second integer.</param>
+/// <param name="base">The base of the numeral system.</param>
 /// <returns></returns>
-vector<int> multiply(vector<int> a, vector<int> b) {
+vector<int> multiply(vector<int> a, vector<int> b, int base) {
 
     vector<int> ret;
     for (int i = 0; i < b.size(); i++) {
@@ -69,8 +90,8 @@ vector<int> multiply(vector<int> a, vector<int> b) {
         for (int ai : a) {
 
             int res = ai * mm + c;
-            int rem = res % 10;
-            c = res / 10;
+            int rem = res % base;
+            c = res / base;
             retCurr.push_back(rem);
         }
         if (c > 0) {
@@ -78,41 +99,38 @@ vector<int> multiply(vector<int> a, vector<int> b) {
         }
 
         // Adding to the main result
-        ret = sum(ret, retCurr);
+        ret = sum(ret, retCurr, base);
     }
 
     return ret;
 }
 
-
-vector<int> power(vector<int> v, int n) {
+vector<int> power1(vector<int> v, int n, int base) {
 
     if (n == 0 || n == 1) {
         return v;
     }
 
     if (n == 2) {
-        return multiply(v, v);
+        return multiply(v, v, base);
     }
 
     int n2 = (int)(log(n) / log(2.));
     int rem = n % ((int)pow(2, n2));
 
-    vector<int> ret = multiply(v, v);
+    vector<int> ret = multiply(v, v, base);
     for (int i = 1; i < n2; i++) {
-        ret = multiply(ret, ret);
+        ret = multiply(ret, ret, base);
     }
 
     for (int i = 0; i < rem; i++) {
-        ret = multiply(ret, v);
+        ret = multiply(ret, v, base);
     }
 
     return ret;
 }
 
-vector<int> powers[1300][500];
-
-vector<int> power(vector<int> v, int iv, int n) {
+vector<int> power2(vector<int> v, int iv, int n, int base) {
 
     if (!powers[iv][n].empty()) {
         return powers[iv][n];
@@ -124,7 +142,7 @@ vector<int> power(vector<int> v, int iv, int n) {
     }
 
     if (n == 2) {
-        powers[iv][n] = multiply(v, v);
+        powers[iv][n] = multiply(v, v, base);
         return powers[iv][n];
     }
 
@@ -132,12 +150,12 @@ vector<int> power(vector<int> v, int iv, int n) {
     int rem = n % ((int)pow(2, n2));
 
     int p = 2; // power
-    vector<int> ret = power(v, iv, p);
+    vector<int> ret = power2(v, iv, p, base);
 
     for (int i = 1; i < n2; i++) {
 
         int vl = iv * iv;
-        ret = power(ret, vl, 2);
+        ret = power2(ret, vl, 2, base);
         p *= 2;
         powers[iv][p] = ret;
 
@@ -154,24 +172,33 @@ vector<int> power(vector<int> v, int iv, int n) {
 
     for (int i = 0; i < rem; i++) {
         p += 1;
-        ret = multiply(ret, v);
+        ret = multiply(ret, v, base);
         powers[iv][p] = ret;
     }
 
     return ret;
 }
 
-vector<int> power11(vector<int> v, int iv, int n) {
+/// <summary>
+/// Recursive version with memorization.
+/// It must be guaranteed that iv is equal to v!
+/// </summary>
+/// <param name="v">The value in vector form.</param>
+/// <param name="iv">The value as an integer (must be equal to iv).</param>
+/// <param name="n">The power.</param>
+/// <param name="base">The base.</param>
+/// <returns></returns>
+vector<int> power3(vector<int> v, int iv, int n, int base) {
 
     if (powers[iv][n].empty()) {
         if (n == 0 || n == 1) {
             powers[iv][n] = v;
         }
         else if (n == 2) {
-            powers[iv][n] = multiply(v, v);
+            powers[iv][n] = multiply(v, v, base);
         }
         else {
-            powers[iv][n] = multiply(power(v, iv, n - 1), v);
+            powers[iv][n] = multiply(power3(v, iv, n - 1, base), v, base);
         }
     }
     return powers[iv][n];
@@ -214,47 +241,15 @@ int sumOfDigits(vector<int> v) {
     int s = 0;
     for (int i : v)
     {
-        s += i;
+        s += sumOfDigits(i, 10);
     }
     return s;
 }
 
 /// <summary>
-/// Solve the problem for the given order and base.
+/// Variant with numbers.
 /// </summary>
-/// <param name="n">The order.</param>
 /// <param name="base">The base.</param>
-void solve(int n, int base) {
-
-    // Min and max numbers for {n, base}
-    long long nmin = (int)pow(base, n);
-    long long nmax = (int)pow(base, n + 1) - 1;
-
-    // Min and max sum of digits for the order n
-    int smin = 2; // always 1, 100 for base=10, n=2
-    int smax = (base - 1) * (n + 1); // 999 for base=10, n=2
-
-    // Loop over the potential sums of digits
-    cout << n << " : ";
-    for (int s = smin; s <= smax; s++) {
-        // Loop over the powers
-        for (int m = 2; m < 300; m++) {
-            int sp = (int)pow(s, m);
-            if (sp < nmin) {
-                continue;
-            }
-            if (sp > nmax) {
-                break;
-            }
-            int sact = sumOfDigits(sp, base);
-            if (sact == s) {
-                cout << sp << " ";
-            }
-        }
-    }
-    cout << endl;
-}
-
 void solveN(int base) {
 
     vector<int> v;
@@ -281,16 +276,21 @@ void solveN(int base) {
     cout << endl;
 }
 
+/// <summary>
+/// Variant with numbers as vectors.
+/// </summary>
+/// <param name="base">The base.</param>
 void solveV(int base) {
 
-    //vector<vector<int>> v;
+    vector<vector<int>> v;
 
     // Max order (100 for base = 10)
     int nmax = static_cast<int>(100. / log10(base));
 
     // Min and max sum of digits for the order n
     int sMin = 2; // always 1, 100 for base=10, n=2
-    int sMax = (base - 1) * nmax; // 999 for base=10, n=2
+    int sBase = sumOfDigits(base - 1, 10);
+    int sMax = sBase * nmax; // 999 for base=10, n=2
 
     // Loop over the potential sums of digits
     int ret = 0;
@@ -301,29 +301,58 @@ void solveV(int base) {
         // cout << "sExp " << sExp << ": ";
         for (int m = 2; m <= mmax; m++) {
             // cout << m << " ";
-            vector<int> sExpR = power11(sExpV, sExp, m); // power(sExpV, m);
+            vector<int> sExpR = power3(sExpV, sExp, m, base);
             int sAct = sumOfDigits(sExpR);
             if (sAct == sExp) {
                 ret++;
-                // cout << sp << " ";
-                // v.push_back(sp);
-                cout << ret << ": " << sExp << " " << m << endl;
+                //cout << sp << " ";
+                //cout << ret << ": " << sExp << " ^ " << m << " : " << endl;
+                //cout << "Base notation:    ";
+                //out(inverse(sExpR));
+                //cout << endl;
+                //vector<int> sExpVDec = convertToVector(sExp, 10);
+                //vector<int> sExpRDec = power1(sExpVDec, m, 10);
+                //cout << "Decimal notation: ";
+                //out(inverse(sExpRDec));
+                //cout << endl;
+                v.push_back(inverse(power1(convertToVector(sExp, 10), m, 10)));
             }
         }
         // cout << endl;
     }
     cout << endl;
-}
 
-void solve(int base) {
-
-    for (int n = 1; n < 100; n++) {
-        solve(n, base);
-    }
+    sort(v.begin(), v.end());
 }
 
 int main() {
 
-    solveV(10);
+    auto start = std::chrono::high_resolution_clock::now();
+
+    for (int base = 2; base <= 1000; base++) {
+
+        cout << endl << "---------- Base " << base << " ----------" << endl;
+
+        auto start1 = std::chrono::high_resolution_clock::now();
+        solveV(base);
+        auto stop1 = std::chrono::high_resolution_clock::now();
+        auto duration1 = std::chrono::duration_cast<std::chrono::microseconds>(stop1 - start1);
+
+        cout << "Time: " << duration1.count() / 1E6 << " seconds" << endl;
+
+        for (int i = 0; i < 1300; i++)
+        {
+            for (int j = 0; j < 500; j++)
+            {
+                powers[i][j].clear();
+            }
+        }
+    }
+
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+
+    cout << "Total time: " << duration.count() / 1E6 << " seconds" << endl;
+
     return 0;
 }
